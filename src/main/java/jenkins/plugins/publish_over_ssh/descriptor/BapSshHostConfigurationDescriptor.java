a44fd8fafd86b7adf71fb32abab1434acd5d1e8c
Watermeyer, Stephan <stephan.watermeyer@dieboldnixdorf.com>
2022-01-25 18:32:09 +0100

    SECURITY-2287: Validate Hostname

--- src/main/java/jenkins/plugins/publish_over_ssh/descriptor/BapSshHostConfigurationDescriptor.java
+++ src/main/java/jenkins/plugins/publish_over_ssh/descriptor/BapSshHostConfigurationDescriptor.java
@@ -25,7 +25,10 @@
 package jenkins.plugins.publish_over_ssh.descriptor;
 
 import java.util.Collections;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.apache.commons.lang.StringUtils;
 import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
@@ -53,6 +56,14 @@ import jenkins.plugins.publish_over_ssh.Messages;
 @Extension
 public class BapSshHostConfigurationDescriptor extends Descriptor<BapSshHostConfiguration> {
 
+	// https://stackoverflow.com/questions/106179/regular-expression-to-match-dns-hostname-or-ip-address
+	private static final Pattern VALID_IP_ADDRESS_PATTERN = Pattern.compile(
+			"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$");
+
+	// https://stackoverflow.com/questions/106179/regular-expression-to-match-dns-hostname-or-ip-address
+	private static final Pattern VALID_HOSTNAME_PATTERN = Pattern.compile(
+			"^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\\-]*[a-zA-Z0-9])\\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\\-]*[A-Za-z0-9])$");
+
 	public BapSshHostConfigurationDescriptor() {
 		super(BapSshHostConfiguration.class);
 	}
@@ -79,7 +90,16 @@ public class BapSshHostConfigurationDescriptor extends Descriptor<BapSshHostConf
 	}
 
 	public FormValidation doCheckHostname(@QueryParameter final String value) {
-		return FormValidation.validateRequired(value);
+		FormValidation retVal = FormValidation.validateRequired(value);
+		if (retVal.kind != FormValidation.Kind.OK) {
+			return retVal;
+		}
+
+		if (isValidHostname(value) || isValidIP(value)) {
+			return FormValidation.ok();
+		}
+
+		return FormValidation.error("Hostname is not a Hostname or IP: " + value);
 	}
 
 	public FormValidation doCheckPort(@QueryParameter final String value) {
@@ -131,6 +151,24 @@ public class BapSshHostConfigurationDescriptor extends Descriptor<BapSshHostConf
 						CredentialsMatchers.instanceOf(BasicSSHUserPrivateKey.class)));
 
 		return retVal;
+	}
+
+	boolean isValidIP(final String pIPAsString) {
+		if (pIPAsString == null || StringUtils.isEmpty(pIPAsString)) {
+			return false;
+		}
+
+		Matcher matcher = VALID_IP_ADDRESS_PATTERN.matcher(pIPAsString);
+		return matcher.matches();
+	}
+
+	boolean isValidHostname(final String pHostnameAsString) {
+		if (pHostnameAsString == null || StringUtils.isEmpty(pHostnameAsString)) {
+			return false;
+		}
+
+		Matcher matcher = VALID_HOSTNAME_PATTERN.matcher(pHostnameAsString);
+		return matcher.matches();
 	}
 
 }
